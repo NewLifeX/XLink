443c9498b289136449f29bd03d337db36575b6bf
大石头 <nnhy@vip.qq.com>
2017-03-16 00:54:49 +0800

    用于模拟设备指令实现

--- xLink.DeviceClient/Properties/AssemblyInfo.cs
+++ xLink.DeviceClient/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("新生命设备客户端")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("用于模拟设备指令实现")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xLink.DeviceClient")]
