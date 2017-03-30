141f356e4d58fe432f5ff788984a5e13542ca145
大石头 <nnhy@vip.qq.com>
2017-03-30 23:37:20 +0800

    新生命物联网平台

---
+++ xLink.Web/Areas/User/UserAreaRegistration.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Web.Mvc;
+using NewLife.Cube;
+
+namespace xLink.Web
+{
+    [DisplayName("用户服务器")]
+    public class UserAreaRegistration : AreaRegistrationBase
+    {
+    }
+}
---
+++ xLink.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WsLink.Front.Web.MvcApplication" Language="C#" %>
