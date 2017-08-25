2be43b1e62884bfd6d5481a355011e16f21bac6a
大石头 <nnhy@vip.qq.com>
2017-08-25 19:56:10 +0800

    使用魔方首页

--- xLink.Web/App_Start/RouteConfig.cs
+++ xLink.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace xLink.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CubeHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
