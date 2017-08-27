0b9e6d69ff72029225e7bda2a93e52c9d70e4a2d
大石头 <nnhy@vip.qq.com>
2017-08-27 17:05:34 +0800

    增加设备指令，支持云端向设备下发指令

---
+++ xLink.Web/Areas/Device/Controllers/DeviceCommandController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using NewLife.Cube;
+using NewLife.Web;
+using xLink.Entity;
+
+namespace xLink.Device.Web.Controllers
+{
+    [Description("设备命令")]
+    public class DeviceCommandController : EntityController<DeviceCommand>
+    {
+
+        /// <summary>列表页视图。子控制器可重载，以传递更多信息给视图，比如修改要显示的列</summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        protected override IEnumerable<DeviceCommand> Search(Pager p)
+        {
+            Boolean? flag = null;
+            if (!p["finished"].IsNullOrEmpty()) flag = p["finished"].ToBoolean();
+
+            return DeviceCommand.Search(p["cmd"], flag, p["dtStart"].ToDateTime(), p["dtEnd"].ToDateTime(), p["Q"], p);
+        }
+    }
+}
