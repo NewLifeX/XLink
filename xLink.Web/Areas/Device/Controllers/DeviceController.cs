4dec7e8d1a5c4f8ebe491ab7594247cb53bbb83c
大石头 <nnhy@vip.qq.com>
2019-09-15 23:34:40 +0800

    Secret

--- xLink.Web/Areas/Device/Controllers/DeviceController.cs
+++ xLink.Web/Areas/Device/Controllers/DeviceController.cs
@@ -13,6 +13,9 @@ namespace xLink.Device.Web.Controllers
         static DeviceController()
         {
             MenuOrder = 80;
+
+            var list = ListFields;
+            list.RemoveField("Secret");
         }
 
         /// <summary>列表页视图。子控制器可重载，以传递更多信息给视图，比如修改要显示的列</summary>
